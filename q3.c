739c4649a812974ba9c2dcfc90fddb2ab04e7e0a
Osnitz <matthieu.pillot@ensea.fr>
2023-11-30 12:05:23 +0100

    q3  12:00

---
+++ q3.c
@@ -0,0 +1,57 @@
+#include "function.h"
+
+void welcome() {
+    write(STDOUT_FILENO, welcome_msg, strlen(welcome_msg));
+    write(STDOUT_FILENO, exit_instruction, strlen(exit_instruction));
+    write(STDOUT_FILENO, msg_enseash, strlen(msg_enseash));
+}
+
+int main(void) {
+    char command[MAXSIZE];
+    int numberOfChar;
+    int status;
+
+    welcome();
+
+    while (1) {
+        // Read user input
+        numberOfChar = read(STDIN_FILENO, command, MAXSIZE);
+        command[numberOfChar - 1] = '\0';
+
+        // Exit if the user types 'exit'
+        if (strcmp(command, "exit") == 0) {
+            write(STDOUT_FILENO, exit_msg, strlen(exit_msg));
+            break;
+        }
+
+        // exit when user type ctrl+D
+        if (numberOfChar == 0 || (numberOfChar == 1 && command[0] == '\n')) {
+            write(STDOUT_FILENO, "Goodbye!\n", strlen("Goodbye!\n"));
+            break;
+        }
+
+        pid_t ret = fork();
+
+        if (ret == 0) {
+            // Child process code
+            execlp(command, command, NULL);
+
+            // If execlp fails, print an error message
+            write(STDOUT_FILENO, error_msg, strlen(error_msg));
+            exit(EXIT_FAILURE);
+        } else {
+            // Parent process code
+            wait(&status);
+
+            // Check if the process terminated normally
+            if (WIFEXITED(status)) {
+                write(STDOUT_FILENO, msg_enseash, strlen(msg_enseash));
+            } else {
+                write(STDOUT_FILENO, "Child process did not terminate normally.\n", strlen("Child process did not terminate normally.\n"));
+            }
+        }
+    }
+
+    return 0;
+}
+
