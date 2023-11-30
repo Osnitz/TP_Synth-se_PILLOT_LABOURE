53d00199f2d4a7e88392638434ad314338b4db8d
Osnitz <matthieu.pillot@ensea.fr>
2023-11-30 09:59:41 +0100

    Q1 update (30/11/2023 10:00)

--- q1.c
+++ q1.c
@@ -2,9 +2,27 @@
 
 void welcome() {
     write(STDOUT_FILENO, welcome_msg, strlen(welcome_msg));
-    write(STDOUT_FILENO, msg_enseash, strlen(msg_enseash));
+    write(STDOUT_FILENO, exit_instruction, strlen(exit_instruction));
+    write(STDOUT_FILENO, msg_enseash , strlen(msg_enseash));
 }
 
-int main (void){
-    welcome();
+int main(void) {
+    char command[MAXSIZE];
+    int numberOfChar;
+    int status;
+
+    while (1) {
+        welcome();
+
+        // Read user input
+        numberOfChar = read(STDIN_FILENO, command, MAXSIZE);
+        command[numberOfChar - 1] = '\0';
+
+        // Exit if the user types 'exit'
+        if (strcmp(command, "exit") == 0) {
+            write(STDOUT_FILENO, exit_msg, strlen(exit_msg));
+            break;
+        }
+    return 0;
+    }
 }
