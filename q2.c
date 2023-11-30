b334257187b256b83392d5b4f65ad96650c9ebee
Osnitz <matthieu.pillot@ensea.fr>
2023-11-30 11:10:39 +0100

    Q2 + Q1 update (30/11/2023 11:02)

--- q2.c
+++ q2.c
@@ -1,7 +1,5 @@
 #include "function.h"
 
-#include "function.h" // Make sure to include your necessary header files
-
 void welcome() {
     write(STDOUT_FILENO, welcome_msg, strlen(welcome_msg));
     write(STDOUT_FILENO, exit_instruction, strlen(exit_instruction));
@@ -11,7 +9,13 @@ void welcome() {
 int main(void) {
     char command[MAXSIZE];
     int numberOfChar;
-    welcome();
+    char *fortune_agrs[] ={"fortune",NULL};
+
+    int status;
+    pid_t ret = fork();
+    if (ret != 0){
+        welcome();
+    }
     while (1) {
         // Read user input
         numberOfChar = read(STDIN_FILENO, command, MAXSIZE);
@@ -22,9 +26,20 @@ int main(void) {
             write(STDOUT_FILENO, exit_msg, strlen(exit_msg));
             break;
         }
+        if((strcmp(command, "fortune") == 0))if (ret== 0){
+                if (ret== 0){
+                //   execvp(fortune_agrs[0],fortune_agrs);
+                execlp(command,command,NULL);
+                }
+                else{
+
+                }
+        }
+
         else{
-        write(STDOUT_FILENO,error_msg, strlen(error_msg));
-        write(STDOUT_FILENO, msg_enseash , strlen(msg_enseash));
+            wait(&status);
+            write(STDOUT_FILENO,error_msg, strlen(error_msg));
+            write(STDOUT_FILENO, msg_enseash , strlen(msg_enseash));
         }
     }
     return 0;
