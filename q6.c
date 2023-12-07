3063b096b9ce933d922c1de5d4335259d50a0ac6
Osnitz <matthieu.pillot@ensea.fr>
2023-12-07 09:46:47 +0100

    q6

--- q6.c
+++ q6.c
@@ -39,6 +39,9 @@ int main(void) {
     int status;
     char buff_exit[MAXSIZE];
     char buff_time[MAXSIZE];
+    char *token;
+    char *args[MAXSIZE];  // Array to store arguments
+    int argument_index;
 
     struct timespec timeStart, timeEnd;
 
@@ -64,11 +67,20 @@ int main(void) {
         clock_gettime(CLOCK_MONOTONIC, &timeStart); // Capture start time
         pid_t ret = fork();
 
-        if (ret == 0) {
-            // Child process code
-            execlp(command, command, NULL);
+        if (ret == 0) {// Child process code
+            token = strtok(command, " "); // Initialize token inside the loop
+            argument_index = 0; // Reset argument_index
 
-            // If execlp fails, print an error message
+            while (token != NULL) {
+                args[argument_index++] = token;
+                token = strtok(NULL, " ");
+            }
+            args[argument_index] = NULL;  // The last element must be NULL for execvp
+
+            // Execute the command with its arguments
+            execvp(args[0], args);
+
+            // If execvp fails, print an error message
             write(STDOUT_FILENO, error_msg, strlen(error_msg));
             exit(EXIT_FAILURE);
         } else {
@@ -77,7 +89,6 @@ int main(void) {
             clock_gettime(CLOCK_MONOTONIC, &timeEnd);
 
             display_execTime_exitCode_prompt(status,buff_exit,buff_time, timeStart,timeEnd);
-
         }
     }
     return 0;
